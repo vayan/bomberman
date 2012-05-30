ad0579dc47522d8f8bdca810616f14054ed9273d
haulot_a <haulot_a@a3b16fbe-e9de-4a39-a64d-d67613dc4cab>
2012-05-30 10:39:59 +0000

    Scoring
    git-svn-id: svn+ssh://koala-rendus.epitech.net/bomberman-2015-haulot_a@31 a3b16fbe-e9de-4a39-a64d-d67613dc4cab

--- src/Bomberman.cpp
+++ src/Bomberman.cpp
@@ -5,7 +5,7 @@
 // Login   <lyvet_r@epitech.net>
 // 
 // Started on  Thu May  3 17:27:27 2012 randy lyvet
-// Last update Wed May 30 12:13:55 2012 alexandre haulotte
+// Last update Wed May 30 12:31:29 2012 alexandre haulotte
 //
 
 #include	<iostream>
@@ -164,11 +164,11 @@ void  Bomberman::update_all_obj(std::list<AObject*>::iterator itb)
 	    if (static_cast<Player*>((*itb))->isDie())
 	      {
 		_audio->play(3);
-		std::cout << "Player have: " << static_cast<Player*>((*itb))->getScore() << std::endl;
 		if (static_cast<Player*>((*itb))->getTy() < 2)
 		  {
 		    Score scr;
 		    scr.checkScore(static_cast<Player*>((*itb))->getScore());
+		    std::cout << "View Hight Score: " << "file:///HightScore.scr.html" << std::endl;
 		  }
 		delete (*itb);
 		this->objects_.erase(itb);
