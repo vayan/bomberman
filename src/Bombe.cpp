4728e2791daeb3b55de32b7ada63130d0451aaf3
vailla_y <vailla_y@a3b16fbe-e9de-4a39-a64d-d67613dc4cab>
2012-05-30 10:28:32 +0000

    git-svn-id: svn+ssh://koala-rendus.epitech.net/bomberman-2015-haulot_a@25 a3b16fbe-e9de-4a39-a64d-d67613dc4cab

--- src/Bombe.cpp
+++ src/Bombe.cpp
@@ -30,23 +30,16 @@ void		Bombe::update(gdl::GameClock const &clock, gdl::Input &_input)
 
 void		Bombe::draw()
 {
-
-      glPushMatrix();
-      glTranslatef(1 * x, 1 * y, 1);
-      glScalef(0.002f, 0.002f, 0.002f);
-      glRotatef(90, 1.0f, 0.0f, 0.0f);
-      this->model_.draw();
-      glPopMatrix();
-      // //      std::list<AObject*>::iterator itb = this->obj.begin();
-      // while (itb != this->obj.end())
-      // 	{
-      // 	  (*itb)->draw();
-      // 	  itb++;
-      // 	}
+  glPushMatrix();
+  glTranslatef(1 * x, 1 * y, 1);
+  glScalef(0.002f, 0.002f, 0.002f);
+  glRotatef(90, 1.0f, 0.0f, 0.0f);
+  this->model_.draw();
+  glPopMatrix();
 }
 
 Bombe::Bombe(Player *j, Bonus *bon, Level *lvl)
-  :AObject(), powa(j->getPower()), lvl(lvl), bonus(bon)
+:AObject(), powa(j->getPower()), lvl(lvl), bonus(bon)
 {
   score = 0;
   x = j->getX();
@@ -57,7 +50,7 @@ Bombe::Bombe(Player *j, Bonus *bon, Level *lvl)
 }
 
 Bombe::Bombe(int &_x, int &_y, int &_powa)
-  :AObject(), powa(_powa), lvl(NULL)
+:AObject(), powa(_powa), lvl(NULL)
 {
   score = 0;
   x = _x;
@@ -87,10 +80,10 @@ int	Bombe::time(bool b)
   if (b == false)
     timer--;
   if (timer <= 0)
-    {
-      timer = -50;
-      return (1);
-    }
+  {
+    timer = -50;
+    return (1);
+  }
   if (b == true)
     timer = 0;
   return (0);
@@ -100,15 +93,15 @@ std::list<AObject*>::iterator Bombe::getObj(std::list<AObject*> &obj, int x_, in
 {
   std::list<AObject*>::iterator itb;
   for (itb = obj.begin(); itb != obj.end(); itb++)
-    {
-      if ((*itb)->getX() == x && (*itb)->getY() == y && (*itb)->_type != -1 && (*itb)->_type != 3 && (*itb)->_type != 4 && (*itb)->_type != 6)
-	return (itb);
-      if ((*itb)->getX() == x_ && (*itb)->getY() == y_ && (*itb)->_type != -1 && (*itb)->_type != 6)
-	{
-	  return (itb);
-	}
-    }
-  return (obj.end());
+  {
+    if ((*itb)->getX() == x && (*itb)->getY() == y && (*itb)->_type != -1 && (*itb)->_type != 3 && (*itb)->_type != 4 && (*itb)->_type != 6)
+     return (itb);
+   if ((*itb)->getX() == x_ && (*itb)->getY() == y_ && (*itb)->_type != -1 && (*itb)->_type != 6)
+   {
+     return (itb);
+   }
+ }
+ return (obj.end());
 }
 
 bool	Bombe::bang(int dir, bool state, std::list<AObject*> &obj, int actBang)
@@ -118,78 +111,78 @@ bool	Bombe::bang(int dir, bool state, std::list<AObject*> &obj, int actBang)
   char	c;
 
   if (state)
+  {
+    tmp = obj.end();
+    x_b = x;
+    y_b = y;
+    switch (dir)
     {
-      tmp = obj.end();
-      x_b = x;
-      y_b = y;
-      switch (dir)
-	{
-	case 0:
-	  x_b -= actBang;
-	  break;
-	case 1:
-	  x_b += actBang;
-	  break;
-	case 2:
-	  y_b -= actBang;
-	  break;
-	case 3:
-	  y_b += actBang;
-	  break;
-	default:
-	  break;
-	}
-      tmp = getObj(obj, x_b, y_b);
-      if (tmp == obj.end())
-	{
-	  return (true);
-	}
+     case 0:
+     x_b -= actBang;
+     break;
+     case 1:
+     x_b += actBang;
+     break;
+     case 2:
+     y_b -= actBang;
+     break;
+     case 3:
+     y_b += actBang;
+     break;
+     default:
+     break;
+   }
+   tmp = getObj(obj, x_b, y_b);
+   if (tmp == obj.end())
+   {
+     return (true);
+   }
+   else
+   {
+     if ((*tmp)->_type != 1)
+     {
+       type = (*tmp)->_type;
+       if ((*tmp)->_type != 2)
+       {
+        if ((*tmp)->_type == 3 || (*tmp)->_type == 4)
+        {
+          static_cast<Bombe*>((*tmp))->time(true);
+          score += 3;
+        }
+        else
+        {
+          delete (*tmp);
+          obj.erase(tmp);
+          score += 1;
+        }
+      }
       else
-	{
-	  if ((*tmp)->_type != 1)
-	    {
-	      type = (*tmp)->_type;
-	      if ((*tmp)->_type != 2)
-		{
-		  if ((*tmp)->_type == 3 || (*tmp)->_type == 4)
-		    {
-		      static_cast<Bombe*>((*tmp))->time(true);
-		      score += 3;
-		    }
-		  else
-		    {
-		      delete (*tmp);
-		      obj.erase(tmp);
-		      score += 1;
-		    }
-		}
-	      else
-		{
-		  static_cast<Player*>((*tmp))->die();
-		  score += 10;
-		}
-	      lvl->setCase(x_b, y_b, 'f');
-	      if (type != 0)
-		return (true);
-	      c = bonus->getBonus(x_b, y_b);
-	      switch (c)
-		{
-		case 'n':
-		  obj.push_back( bonus->createBonus(x_b, y_b, 1, 3));
-		  break;
-		case 'p':
-		  obj.push_back( bonus->createBonus(x_b, y_b, 1, 1));
-		  break;
-		case 's':
-		  obj.push_back( bonus->createBonus(x_b, y_b, 1, 2));
-		  break;
-		default:
-		  break;
-		}
-	    }
-	}
+      {
+        static_cast<Player*>((*tmp))->die();
+        score += 10;
+      }
+      lvl->setCase(x_b, y_b, 'f');
+      if (type != 0)
+        return (true);
+      c = bonus->getBonus(x_b, y_b);
+      switch (c)
+      {
+        case 'n':
+        obj.push_back( bonus->createBonus(x_b, y_b, 1, 3));
+        break;
+        case 'p':
+        obj.push_back( bonus->createBonus(x_b, y_b, 1, 1));
+        break;
+        case 's':
+        obj.push_back( bonus->createBonus(x_b, y_b, 1, 2));
+        break;
+        default:
+        break;
+      }
     }
-  return (false);
+  }
+}
+return (false);
 }
 
 void	Bombe::explose(std::list<AObject*> &obj)
@@ -198,19 +191,19 @@ void	Bombe::explose(std::list<AObject*> &obj)
   bool	state[4];
 
   AObject *kaboom = new Explosion(checkPath(1 ,getPowa()),
-				  checkPath(2 ,getPowa()),
-				  checkPath(3 ,getPowa()),
-				  checkPath(4 ,getPowa()), this->x, this->y);
+    checkPath(2 ,getPowa()),
+    checkPath(3 ,getPowa()),
+    checkPath(4 ,getPowa()), this->x, this->y);
   obj.push_back(kaboom);
   for (i = 0; i < 4; i++)
     state[i] = true;
   for (i = 1; i <= powa; i++)
+  {
+    for (int j = 0; j < 4; j++)
     {
-      for (int j = 0; j < 4; j++)
-	{
-	  state[j] = bang(j, state[j], obj, i);
-	}
-    }
+     state[j] = bang(j, state[j], obj, i);
+   }
+ }
 }
 
 
@@ -219,8 +212,8 @@ std::string	Bombe::pack(Bombe* b)
   std::string	str;
 
   str = to_string(b->getX()) + ";"
-    + to_string(b->getY()) + ";"
-    + to_string(b->getPowa());
+  + to_string(b->getY()) + ";"
+  + to_string(b->getPowa());
   return (str);
 }
 
@@ -247,47 +240,47 @@ int                   Bombe::checkPath(int direction, int power)
   int   go = -1;
 
   switch (direction)
-    {
+  {
     case 1 :
-      while (++go < power && ++y)
-	{
-	  if (this->lvl->getCase(x, y) == 'w')
-	    return (go);
-	  if (this->lvl->getCase(x, y) == 'd' )
-	    return (go + 1);
-	}
-      break;
-    case 2 :
-      while (++go < power && --y)
-	{
-	  if (this->lvl->getCase(x, y) == 'w')
-	    return (go);
-	  if (this->lvl->getCase(x, y) == 'd' )
-	    return (go + 1);
-	}
-      break;
-    case 3 :
-      while (++go < power && --x)
-	{
-	  if (this->lvl->getCase(x, y) == 'w')
-	    return (go);
-	  if (this->lvl->getCase(x, y) == 'd' )
-	    return (go + 1);
-	}
-      break;
-    case 4 :
-      while (++go < power && ++x)
-	{
-	  if (this->lvl->getCase(x, y) == 'w')
-	    return (go);
-	  if (this->lvl->getCase(x, y) == 'd' )
-	    return (go + 1);
-	}
-      break;
-    default :
-      return (0);
-    }
-  return (go);
+    while (++go < power && ++y)
+    {
+     if (this->lvl->getCase(x, y) == 'w')
+       return (go);
+     if (this->lvl->getCase(x, y) == 'd' )
+       return (go + 1);
+   }
+   break;
+   case 2 :
+   while (++go < power && --y)
+   {
+     if (this->lvl->getCase(x, y) == 'w')
+       return (go);
+     if (this->lvl->getCase(x, y) == 'd' )
+       return (go + 1);
+   }
+   break;
+   case 3 :
+   while (++go < power && --x)
+   {
+     if (this->lvl->getCase(x, y) == 'w')
+       return (go);
+     if (this->lvl->getCase(x, y) == 'd' )
+       return (go + 1);
+   }
+   break;
+   case 4 :
+   while (++go < power && ++x)
+   {
+     if (this->lvl->getCase(x, y) == 'w')
+       return (go);
+     if (this->lvl->getCase(x, y) == 'd' )
+       return (go + 1);
+   }
+   break;
+   default :
+   return (0);
+ }
+ return (go);
 }
 
 Bombe::~Bombe()
